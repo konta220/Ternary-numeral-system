f8b29ddea3a895fefd685de7a34874f66f1d928c
konta220 <konta220@users.noreply.github.com>
2018-10-11 12:09:59 +0900

    substract

--- Adder/python-sample/substract.py
+++ Adder/python-sample/substract.py
@@ -3,10 +3,10 @@ import unittest
 # A-B  (A>B)
 
 # 0b00111
-A = 7
+A = 12
 
 # 0b00011
-B = 3
+B = 5
 
 # bit count
 N = 0
@@ -40,12 +40,13 @@ def Tn(num):
         return 0
 
     return -(1 if Hn(num) < 0 else 0) + \
-        (0 if Hn(num) < 0 else 1) *\
-        (Sn(num) + (1 if (not a_Bit(Tn(num-1) * b_Bit(num-1))) else 0) * Tn(num-1))
+        (1 if not Hn(num) < 0 else 0) *\
+        (Sn(num) + (1 if (not a_Bit(Tn(num-1)) and b_Bit(Tn(num-1))) else 0) * Tn(num-1))
 
 
 def Rn(num):
-    return abs(Tn(num)) if (a_Bit(Tn(num-1)) or (not b_Bit(Tn(num-1)))) else 0
+    binary = (a_Bit(Tn(num-1)) or (not b_Bit(Tn(num-1))))
+    return abs(Tn(num)) if binary else 0
 
 
 if __name__ == '__main__':
