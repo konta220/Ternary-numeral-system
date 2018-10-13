35bee4bdecaf28e82a3efa91d26916aa4a5b5550
konta220 <konta220@users.noreply.github.com>
2018-10-13 12:07:27 +0900

    正 + 負　における加算方法について計算

--- Adder/python-sample/substract.py
+++ Adder/python-sample/substract.py
@@ -1,17 +1,5 @@
 import unittest
 
-# A-B  (A>B)
-
-# 0b00111
-A = 12
-
-# 0b00011
-B = 5
-
-# bit count
-N = 0
-MAX = 5
-
 
 def a_Bit(num):
     return num > 0
@@ -39,24 +27,49 @@ def Tn(num):
     if num < 0:
         return 0
 
-    return -(1 if Hn(num) < 0 else 0) + \
+    return Hn(num) + \
         (1 if not Hn(num) < 0 else 0) *\
         (Sn(num) + (1 if (not a_Bit(Tn(num-1)) and b_Bit(Tn(num-1))) else 0) * Tn(num-1))
 
 
 def Rn(num):
-    binary = (a_Bit(Tn(num-1)) or (not b_Bit(Tn(num-1))))
+    binary = (not (b_Bit(Sn(num)) and b_Bit(Tn(num-1))))
     return abs(Tn(num)) if binary else 0
 
 
-if __name__ == '__main__':
+def Substract():
+
+    A_sub_B = 0
+    for num in range(A):
+        A_sub_B += Rn(num) << num
+
+    if A_sub_B != (A-B):
+        printResult(A_sub_B)
+
+
+def printResult(A_sub_B):
     print("{0}: {1} {2}".format("A", A, bin(A)))
     print("{0}: {1} {2}".format("B", B, bin(B)))
     print("A - B: {0} {1}".format(A-B, bin(A-B)))
     print("")
 
-    print("result")
-    print(Rn(3))
-    print(Rn(2))
-    print(Rn(1))
-    print(Rn(0))
+    print("result : {0}{1}{2}{3}".format(Rn(3), Rn(2), Rn(1), Rn(0)))
+
+    print(A_sub_B == (A-B))
+    print(bin(A_sub_B))
+
+
+if __name__ == '__main__':
+    # A-B  (A>B)
+    
+    A=1021
+    B=365
+
+    # MAX_NUM = 1024
+    # for A in range(MAX_NUM+1):
+    #     for B in range(A/2, A+1):
+    #         Substract()
+    #     print(A)
+
+    print()
+    print("Done")
